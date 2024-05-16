9533833708bbb57e34c7dc616fa7f79db594255c
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2024-05-16 01:13:53 +0100

    changes to some of the homepages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import { IoMdSearch } from 'react-icons/io'
 
 export const Homepage = () => {
   return (
-    <main>
+    <main className='homePageMain'>
       <header className='navStyle'>
         <nav className='navStyle'>
           <img src="/Media Hub Logo 2 1.png" alt="logo" />
